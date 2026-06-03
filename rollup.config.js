8844d13ee34bcb6e03e78178309920a17cda550c
enaden711 <rosedenadel@gmail.com>
2026-06-03 11:11:45 +0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,21 @@
-.pedantry/rollup.config.js
+.pedantry/ke ep3 64öä37 3i3l3ö37  ei rollup.config.js
+B4
+r
+til
+en
+en
+rkolr
+k
+El
+e. k
+emk
+en. fnlotn ti47i5n4i4 t 
+3
+d
+r
+f
+8
+ok
+?
+true but it's still available for the next week as all. 
+true good
